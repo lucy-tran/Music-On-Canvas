63f05b3e6c5d404df0e3bc5ba77429e1f7fdee30
Lucy Tran <ltran2@macalester.edu>
2022-11-21 15:52:30 -0600

    Add header and copied code from OpenAI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [animalInput, setAnimalInput] = useState("");
+	const [result, setResult] = useState();
+
+	async function onSubmit(event) {
+		event.preventDefault();
+		const response = await fetch("/api/generate", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ animal: animalInput }),
+		});
+		const data = await response.json();
+		setResult(data.result);
+		setAnimalInput("");
+	}
+	return (
+		<div className="App">
+			<header className="App-header">
+				<a className="App-logo" href="./">
+					Music On Canvas
+				</a>
+			</header>
+			<main className="main">
+				<form onSubmit={onSubmit}>
+					<input
+						type="text"
+						name="animal"
+						placeholder="Enter a prompt"
+						value={animalInput}
+						onChange={(e) => setAnimalInput(e.target.value)}
+					/>
+					<input type="submit" value="Generate names" />
+				</form>
+				<div className="result">{result}</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
