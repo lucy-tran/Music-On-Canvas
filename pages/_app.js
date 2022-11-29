87594f1d2aaa77d8920645e6672c74a948e82cf3
Lucy Tran <ltran2@macalester.edu>
2022-11-29 14:56:53 -0600

    Added root _app file and the primary text font (Inter)

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import "../styles/global.scss";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function App({ Component, pageProps }) {
+	return (
+		<main className={inter.className}>
+			<Component {...pageProps} />
+		</main>
+	);
+}
