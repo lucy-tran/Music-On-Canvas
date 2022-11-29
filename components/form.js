3c652167dd85ad016593b851e765c281eafd1643
Lucy Tran <ltran2@macalester.edu>
2022-11-29 14:49:12 -0600

    Completed Form component

---
+++ components/form.js
@@ -0,0 +1,71 @@
+import styles from "./form.module.scss";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { validateTrack } from "../lib/tracks";
+
+export default function Form({ home, defaultValue, optionAction }) {
+	const [trackInput, setTrackInput] = useState(
+		defaultValue ? defaultValue : ""
+	);
+	const [errorMessage, setErrorMessage] = useState();
+	const router = useRouter();
+
+	async function onSubmit(event) {
+		event.preventDefault();
+		let [track, artist] = trackInput.split("by");
+		if (!track || !artist || track.trim() || artist.trim()) {
+			setErrorMessage(
+				"* Incorrect syntax. Please follow the format (without the brackets)."
+			);
+			return;
+		}
+		track = track.trim();
+		artist = artist.trim();
+		console.log("track: " + track);
+		console.log("artist: " + artist);
+
+		const validateRes = await validateTrack(track, artist);
+
+		if (validateRes.error) {
+			setErrorMessage(validateRes.error);
+			return;
+		}
+
+		router.push({
+			pathname: "/tracks/[mbid]",
+			query: { mbid: validateRes.mbid },
+		});
+	}
+
+	return (
+		<div className={styles.wrapper}>
+			<div className={`${styles.header} ${!home && styles.alignLeft}`}>
+				Enter a [track title] + " by " + [artist]
+			</div>
+			<form className={styles.form} onSubmit={onSubmit}>
+				<input
+					type="text"
+					placeholder="I'm not the only one by Sam Smith"
+					value={trackInput}
+					onChange={(e) => {
+						setTrackInput(e.target.value);
+						setErrorMessage("");
+					}}
+					className={styles.textInput}
+				/>
+				<button tabIndex={0} type="submit" className={styles.submitButton}>
+					Draw
+				</button>
+			</form>
+			{errorMessage && (
+				<div className={styles.errorMessage}>{errorMessage}</div>
+			)}
+			<div className={`${styles.randomize} ${!home && styles.alignLeft}`}>
+				or
+				<a onClick={optionAction}>
+					{home ? "randomize a track" : "choose from our suggestions"}
+				</a>
+			</div>
+		</div>
+	);
+}
