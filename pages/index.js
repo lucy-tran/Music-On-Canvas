5a31d0f0c7a9b5bcb20be9600f7cd211ae0c1d47
Lucy Tran <ltran2@macalester.edu>
2022-11-29 14:59:47 -0600

    Fleshed out the Home page, html and styling. In progress.

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import styles from "../styles/Home.module.scss";
+import utilStyles from "../styles/utils.module.scss";
+import Image from "next/image";
+import Head from "next/head";
+import Logo from "../components/logo";
+import NavItem from "../components/navItem";
+import Form from "../components/form";
+
+export const siteTitle = "Music On Canvas";
+
+export default function Home() {
+	function randomize() {}
+
+	return (
+		<div className={utilStyles.layout}>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<header className={utilStyles.header}>
+				<div>
+					<NavItem page="about">About</NavItem>
+					<NavItem page="exhibition">Exhibition</NavItem>
+				</div>
+				<div className={utilStyles.accountIcon}>
+					<Image
+						src="/images/account.jpg"
+						layout="fill"
+						className={utilStyles.accountImg}
+					/>
+				</div>
+			</header>
+			<div className={styles.logoContainer}>
+				<Logo home />
+			</div>
+			<section className={styles.main}>
+				<Form home optionAction={randomize} />
+			</section>
+		</div>
+	);
+}
