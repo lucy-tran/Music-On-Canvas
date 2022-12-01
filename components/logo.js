d43cfcb629c1e8ab9b0bd65f5f97ce38ad89f2ad
Lucy Tran <ltran2@macalester.edu>
2022-12-01 00:24:58 -0600

    Added font and router to home page

--- components/logo.js
+++ components/logo.js
@@ -1,22 +1,25 @@
 import styles from "./logo.module.scss";
 import {
 	Arima,
-	MuseoModerno,
-	Grenze_Gotisch,
-	Cairo_Play,
-	Nabla,
+	// MuseoModerno,
+	// Grenze_Gotisch,
+	// Cairo_Play,
+	// Nabla,
 } from "@next/font/google";
+import { useRouter } from "next/router";
 
 const arima = Arima({ subsets: ["latin"] });
-const museoModerno = MuseoModerno({ subsets: ["latin"] });
-const grenze_Gotisch = Grenze_Gotisch({ subsets: ["latin"] });
-const cairo_Play = Cairo_Play({ subsets: ["latin"] });
-const nabla = Nabla({ subsets: ["latin"] });
+// const museoModerno = MuseoModerno({ subsets: ["latin"] });
+// const grenze_Gotisch = Grenze_Gotisch({ subsets: ["latin"] });
+// const cairo_Play = Cairo_Play({ subsets: ["latin"] });
+// const nabla = Nabla({ subsets: ["latin"] });
 
 export default function Logo({ home }) {
+	const router = useRouter();
 	return (
 		<p
 			className={`${styles.logo} ${arima.className} ${home && styles.homeLogo}`}
+			onClick={() => router.push("/")}
 		>
 			Music On Canvas
 		</p>
