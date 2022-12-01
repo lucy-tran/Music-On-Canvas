b5e698f1fde3c2c88a80703a19d8c65f2568559a
Lucy Tran <ltran2@macalester.edu>
2022-12-01 00:26:59 -0600

    Added domains for absolute image URLs

--- next.config.js
+++ next.config.js
@@ -8,4 +8,10 @@ module.exports = {
 		],
 		prependData: `@import "~/styles/variables.scss";`,
 	},
+	images: {
+		domains: [
+			"via.placeholder.com",
+			"oaidalleapiprodscus.blob.core.windows.net",
+		],
+	},
 };
